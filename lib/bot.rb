e7edecb214f0fac9516e882647a7086148c43b4c
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-04 10:48:24 +0100

    Add token

---
+++ lib/bot.rb
@@ -0,0 +1,14 @@
+require 'telegram/bot'
+
+token = '1156943187:AAE_JHdRfLYEWxUw5R30vYgng_-TONyDMtA'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
