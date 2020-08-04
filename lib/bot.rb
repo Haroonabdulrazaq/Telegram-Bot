6c0a403401ecd7511939d99535c38deac19f1f60
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-04 14:20:29 +0100

    call the exchange api response in bot.rb

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,14 +1,30 @@
 require 'telegram/bot'
+require_relative 'exchange.rb'
 
-token = '1156943187:AAE_JHdRfLYEWxUw5R30vYgng_-TONyDMtA'
-
-Telegram::Bot::Client.run(token) do |bot|
-  bot.listen do |message|
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+class ExchangeBOt
+    exchanger = Exchange.new
+    def initialize  
+        @token = '1156943187:AAE_JHdRfLYEWxUw5R30vYgng_-TONyDMtA'
+        init_bot
     end
-  end
+    
+    def init_bot
+        Telegram::Bot::Client.run(@token) do |bot|
+            botCommands
+        end
+    end
+
+    def botCommands
+        bot.listen do |message|
+            case message.text
+            when '/start'
+            bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+            when '/exchangebot'
+            bot.api.send_message(chat_id: message.chat.id, text: "The exchange rate for all currency is, #{exchanger.get_request}")
+            when '/stop'
+            bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+            end    
+        end
+    end
 end
+
--- lib/exchange.rb
+++ lib/exchange.rb
@@ -5,32 +5,27 @@ require 'json'
 
 
 class Exchange
-  
+
     def get_request
-    url = URI("https://currency-value.p.rapidapi.com/global/currency_rates")
+        url = URI("https://currency-value.p.rapidapi.com/global/currency_rates")
 
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    request = Net::HTTP::Get.new(url)
-    request["x-rapidapi-host"] = 'currency-value.p.rapidapi.com'
-    request["x-rapidapi-key"] = '5fbec538d7msh9e5378e39f8e570p164c8ajsnf8c7f46f4df4'
+        request = Net::HTTP::Get.new(url)
+        request["x-rapidapi-host"] = 'currency-value.p.rapidapi.com'
+        request["x-rapidapi-key"] = '5fbec538d7msh9e5378e39f8e570p164c8ajsnf8c7f46f4df4'
 
-    
+        response = http.request(request)
+        currency_value = JSON.parse(response.body)
 
-    response = http.request(request)
-    dictionary = { "one" => "eins", "two" => "zwei", "three" => "drei" }
-    currency_value = JSON.parse(response.body)
-
-    currency_value["currency_rates"].each do |currency_ratings |
-        puts "#{currency_ratings} to USD\n\n"
-    end
+            currency_value["currency_rates"].each do |currency_ratings |
+            bot_response "#{currency_ratings[0]} #{currency_ratings[1]} equals 1 USD\n\n"
+            bot_response
+            end
 
     end
 
 end
 
-exchanger = Exchange.new
-
-puts exchanger.get_request
