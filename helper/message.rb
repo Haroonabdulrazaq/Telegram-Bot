64a2629aff528458cc24122a815c6a9615cba533
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-05 18:25:37 +0100

    Fix linters err

--- helper/message.rb
+++ helper/message.rb
@@ -1,5 +1,5 @@
 module Message
-WELCOME_MESSAGE=' I am your Friendly Bot exchanger. 
+  WELCOME_MESSAGE = ' I am your Friendly Bot exchanger.
 I can help you with the most updated information on currency exchange, Like.
 1. Rate of exchange in comparison to dollar
 2. Value of currency in comparison to dollar
@@ -8,30 +8,26 @@ I can help you with the most updated information on currency exchange, Like.
 Input this /commands to see valid commmands to interact with me
 '.freeze
 
-BYE_MESSAGE = '
+  BYE_MESSAGE = '
 I was designed by Haroon Abdulrazaq you can reach him on:
 Gmail: Haroonabdulrazaq@gmail.com
 Twitter: @Hanq_o
 LinkedIn: https://www.linkedin.com/in/haroon-abdulrazaq-817906100/
 Thanks for interacting with me, hope to see you soon!'
-WARNING_MESSAGE = 'Oopps!!, It seems you have entered an invaild command, you should input /commands to see the valid commands for this bot'.freeze
-VALID_COMMANDS = 'Enter any of the following commands and I will reply you as soon as possible
+  WARNING_MESSAGE = 'Oopps!!, It seems you have entered an invaild command,
+  you should input /commands to see the valid commands for this bot'
+  VALID_COMMANDS = 'Enter any of the following commands and I will reply you as soon as possible
 I will reply you with the returns specified next to every message:
 1. Input : /start    =>  returns : This command starts the bot
 2. Input : /commands    =>  returns : List of all commands
 3. Input : /explore =>  returns : List of all available currency---Note It\'s a  lot of data
 4. Input : usd-eur(example)     =>  response: with the usd/eur exchange rate, you can use any currency of your choice
-5. Input : /formular  =>  returns : The calculation involve in the exchange rate
+5. Input : /formula  =>  returns : The calculation involve in the exchange rate
 6. Input : /stop   =>  responds : with a Good bye message to user
-'.freeze
-FORMULA1 = 'Formula for rate of exchange is
+'
+  FORMULA1 = 'Formula for rate of exchange is
      '
-FORMULA2 = '
-Formula for value of currency is 
+  FORMULA2 = '
+Formula for value of currency is
      usd_rate/currency_rate'
-
-
-MORE_SPACE = '
-     
-     '
 end
--- lib/bot.rb
+++ lib/bot.rb
@@ -2,62 +2,66 @@ require 'telegram/bot'
 require_relative 'exchange.rb'
 require_relative '../helper/message.rb'
 require 'dotenv'
-  Dotenv.load
+Dotenv.load
+# Exchange Bot Class
+class ExchangeBot
+  def initialize
+    @token = ENV['BOT_TOKEN']
+    init_bot
+  end
 
-class ExchangeBot  
-    def initialize
-        @token = ENV['BOT_TOKEN']
-        init_bot
-    end
-    
- private def init_bot
-        Telegram::Bot::Client.run(@token) do |bot|
-            botCommands(bot)
-        end
-    end
+  private
 
-    def botCommands(bot)
-        exchanger = Exchange.new
-        explore_arr= []
-        bot.listen do |message|
-        case message.text
-            when '/start'
-                bot.api.send_message(chat_id: message.chat.id, text: "Hi there #{message.from.first_name}," << Message::WELCOME_MESSAGE)       
-            when '/commands'
-                bot.api.send_message(chat_id: message.chat.id, text: Message::VALID_COMMANDS)
-            when '/explore'
-                whole_obj = exchanger.get_request["currency_rates"]
-                 whole_obj.each do |key,i|
-                    break if i == 30
-                     explore_arr << " #{key} "
-                     i +=1
-                 end
-                bot.api.send_message(chat_id: message.chat.id, text: "#{explore_arr}")
-            when '/formula'
-                formula = exchanger.get_request["formula"]
-                bot.api.send_message(chat_id: message.chat.id, text: Message::FORMULA1<<" #{formula}"<< Message::FORMULA2 )
-            when /usd-([a-z]{3})/i
-                users_query = message.text 
-                calculation =0
-                if users_query.include?('-')
-                    valid_input = users_query.split('-')
-                    currency_rate = exchanger.get_request["currency_rates"]
-                    usd_rate = currency_rate[valid_input[0].upcase].to_f
-                    eur_rate = currency_rate[valid_input[1].upcase].to_f 
-                    if usd_rate != 0 && eur_rate !=0
-                        calculation = usd_rate / eur_rate
-                        bot.api.send_message(chat_id: message.chat.id, text: "1 #{valid_input[0]} equals #{calculation} #{valid_input[1]}")
-                        bot.api.send_message(chat_id: message.chat.id, text: "#{valid_input[0]} to #{valid_input[1]} rate is, #{usd_rate} - #{eur_rate}")
-                    else
-                        bot.api.send_message(chat_id: message.chat.id, text: Message::WARNING_MESSAGE)
-                    end   
-                end
-            when '/stop'
-            bot.api.send_message(chat_id: message.chat.id, text: Message::BYE_MESSAGE << " #{message.from.first_name}") 
-            else
-                bot.api.send_message(chat_id: message.chat.id, text: Message::WARNING_MESSAGE)
-            end
-        end
+  def init_bot
+    Telegram::Bot::Client.run(@token) do |bot|
+      bot_commands(bot)
     end
+  end
+
+  def bot_commands(bot)
+    exchanger = Exchange.new
+    explore_arr = []
+    bot.listen do |message|
+      case message.text
+      when '/start'
+        bot.api.send_message(chat_id: message.chat.id,
+                             text: "Hi there #{message.from.first_name}," << Message::WELCOME_MESSAGE)
+      when '/commands'
+        bot.api.send_message(chat_id: message.chat.id, text: Message::VALID_COMMANDS)
+      when '/explore'
+        whole_obj = exchanger.make_request['currency_rates']
+        whole_obj.each do |key, i|
+          break if i == 30
+
+          explore_arr << " #{key} "
+        end
+        bot.api.send_message(chat_id: message.chat.id, text: explore_arr.to_s)
+      when '/formula'
+        formula = exchanger.make_request['formula']
+        bot.api.send_message(chat_id: message.chat.id, text: Message::FORMULA1 << " #{formula}" << Message::FORMULA2)
+      when /usd-([a-z]{3})/i
+        users_query = message.text
+        # calculation = 0
+        if users_query.include?('-')
+          valid_input = users_query.split('-')
+          currency_rate = exchanger.make_request['currency_rates']
+          usd_rate = currency_rate[valid_input[0].upcase].to_f
+          eur_rate = currency_rate[valid_input[1].upcase].to_f
+          if usd_rate != 0 && eur_rate != 0
+            calculation = usd_rate / eur_rate
+            bot.api.send_message(chat_id: message.chat.id,
+                                 text: "1 #{valid_input[0]} equals #{calculation} #{valid_input[1]}")
+            bot.api.send_message(chat_id: message.chat.id,
+                                 text: "#{valid_input[0]} to #{valid_input[1]} rate is, #{usd_rate} - #{eur_rate}")
+          else
+            bot.api.send_message(chat_id: message.chat.id, text: Message::WARNING_MESSAGE)
+          end
+        end
+      when '/stop'
+        bot.api.send_message(chat_id: message.chat.id, text: Message::BYE_MESSAGE << " #{message.from.first_name}")
+      else
+        bot.api.send_message(chat_id: message.chat.id, text: Message::WARNING_MESSAGE)
+      end
+    end
+  end
 end
- 
