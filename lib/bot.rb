cb7fffbf0e0a140834a3638f6f9b25315afc0a89
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-05 13:38:21 +0100

    Add environmental variable

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,11 +1,13 @@
 require 'telegram/bot'
 require_relative 'exchange.rb'
-require_relative 'message.rb'
+require_relative '../helper/message.rb'
+require 'dotenv'
+  Dotenv.load
 
 class ExchangeBot  
-    
+
     def initialize
-        @token = '1156943187:AAE_JHdRfLYEWxUw5R30vYgng_-TONyDMtA'
+        @token = ENV['BOT_TOKEN']
         init_bot
     end
     
@@ -16,14 +18,17 @@ class ExchangeBot
     end
 
     def botCommands(bot)
+        exchanger = Exchange.new
         bot.listen do |message|
             case message.text
             when '/start'
-            bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")               
+                bot.api.send_message(chat_id: message.chat.id, text: "Hi there #{message.from.first_name}," << Message::WELCOME_MESSAGE)       
             when '/commands'
-            bot.api.send_message(chat_id: message.chat.id, text: Message::VALID_COMMANDS)
-            when /usd-[a-z]{3}/i
-                exchanger = Exchange.new
+                bot.api.send_message(chat_id: message.chat.id, text: Message::VALID_COMMANDS)
+            when '/formula'
+                formula = exchanger.get_request["formula"]
+                bot.api.send_message(chat_id: message.chat.id, text: Message::FORMULA1<<" #{formula}"<< Message::FORMULA2 )
+            when /usd-([a-z]{3})/i
                 users_query = message.text 
                 calculation =0
                 if users_query.include?('-')
@@ -34,8 +39,10 @@ class ExchangeBot
                     if usd_rate != 0 && eur_rate !=0
                         calculation = usd_rate / eur_rate
                         bot.api.send_message(chat_id: message.chat.id, text: "1 #{valid_input[0]} equals #{calculation} #{valid_input[1]}")
-                    end
-                    bot.api.send_message(chat_id: message.chat.id, text: "#{valid_input[1]} to #{valid_input[0]} rate is, #{usd_rate} - #{eur_rate}") 
+                        bot.api.send_message(chat_id: message.chat.id, text: "#{valid_input[0]} to #{valid_input[1]} rate is, #{usd_rate} - #{eur_rate}")
+                    else
+                        bot.api.send_message(chat_id: message.chat.id, text: Message::WARNING_MESSAGE)
+                    end   
                 end
             when '/stop'
             bot.api.send_message(chat_id: message.chat.id, text: Message::BYE_MESSAGE << " #{message.from.first_name}") 
