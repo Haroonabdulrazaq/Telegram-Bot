148c24ca53b629fdfe51b0d41e40df5d0c80a77f
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-05 09:58:34 +0100

    Add regex to the botcommand

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 gem 'rubocop', '~>0.81.0'
 gem 'json', '~> 2.3', '>= 2.3.1'
+gem 'dotenv', '~> 2.1', '>= 2.1.1'
--- lib/bot.rb
+++ lib/bot.rb
@@ -19,25 +19,25 @@ class ExchangeBot
             case message.text
             when '/start'
             bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")               
-            when '/exchangebot'
+            when '/Commands'
                 exchanger = Exchange.new
             bot.api.send_message(chat_id: message.chat.id, text: "The exchange rate for all currency is, #{exchanger.format_response}")
             when '/stop'
             bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-            else
+            when /[a-z]-[a-z]/
               exchanger = Exchange.new
               users_query = message.text 
               calculation =0
-                if users_query.include?('/')
-                    valid_input = users_query.split('/')
+                if users_query.include?('-')
+                    valid_input = users_query.split('-')
                     currency_rate = exchanger.get_request["currency_rates"]
                     usd_rate = currency_rate[valid_input[0].upcase].to_f
                     eur_rate = currency_rate[valid_input[1].upcase].to_f 
-                    if usd_rate != 0 && eur_rate != 0
+                    if usd_rate != 0 && eur_rate !=0
                         calculation = usd_rate / eur_rate
                         bot.api.send_message(chat_id: message.chat.id, text: "1 #{valid_input[0]} equals #{calculation} #{valid_input[1]}")
                     end
-                    bot.api.send_message(chat_id: message.chat.id, text: "#{valid_input[1]} to #{valid_input[0]} rate is, #{usd_rate} / #{eur_rate}") 
+                    bot.api.send_message(chat_id: message.chat.id, text: "#{valid_input[1]} to #{valid_input[0]} rate is, #{usd_rate} - #{eur_rate}") 
                 end
             end    
         end
