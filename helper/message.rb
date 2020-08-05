9633fe29749f5fbca3300d1cbb21483e08abce6d
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-05 15:08:40 +0100

    Update the message user gets

--- helper/message.rb
+++ helper/message.rb
@@ -16,13 +16,13 @@ LinkedIn: https://www.linkedin.com/in/haroon-abdulrazaq-817906100/
 Thanks for interacting with me, hope to see you soon!'
 WARNING_MESSAGE = 'Oopps!!, It seems you have entered an invaild command, you should input /commands to see the valid commands for this bot'.freeze
 VALID_COMMANDS = 'Enter any of the following commands and I will reply you as soon as possible
-I will reply you with the response specified next to every message:
-1. Input : /start    =>  response : Starts the bot
-2. Input : /commands    =>  response : List of all commands
-3. Input : /explore =>  response : List of all available currency---Note It\'s a  lot of data
-4. Input : usd-eur(example)     =>  response : with the usd/eur exchange rate, you can use any currency of your choice
-5. Input : /formular  =>  response : The calculation involve in the exchange rate
-6. Input : /stop   =>  response : Send a Good bye message to user
+I will reply you with the returns specified next to every message:
+1. Input : /start    =>  returns : This command starts the bot
+2. Input : /commands    =>  returns : List of all commands
+3. Input : /explore =>  returns : List of all available currency---Note It\'s a  lot of data
+4. Input : usd-eur(example)     =>  response: with the usd/eur exchange rate, you can use any currency of your choice
+5. Input : /formular  =>  returns : The calculation involve in the exchange rate
+6. Input : /stop   =>  responds : with a Good bye message to user
 '.freeze
 FORMULA1 = 'Formula for rate of exchange is
      '
